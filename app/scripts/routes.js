dc1e2dbfac1dd7d2fbd9f6462daf3103245f6553
Rob Thijssen <rthijssen@gmail.com>
2014-08-30 17:36:32 +0100

    firebase simple login

--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -7,6 +7,14 @@ app.config(function ($routeProvider) {
       templateUrl: 'views/home.html',
       controller: 'HomeCtrl'
     })
+    .when('/register', {
+      templateUrl: 'views/register.html',
+      controller: 'AuthCtrl'
+    })
+    .when('/login', {
+      templateUrl: 'views/login.html',
+      controller: 'AuthCtrl'
+    })
     .when('/people', {
       templateUrl: 'views/persons.html',
       controller: 'PersonsCtrl'
