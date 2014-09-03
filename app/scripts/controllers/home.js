c0e984839d3d8323046e36a166b6af9a541ee113
Rob Thijssen <rthijssen@gmail.com>
2014-09-03 16:33:53 +0100

    implemented elastic search request / response queues.

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,19 +1,11 @@
 'use strict';
 
-app.controller('HomeCtrl', function ($scope, Person) {
-  $scope.currentPerson = {};
-  $scope.persons = Person.all;
-  $scope.query = '';
-  var regex;
-  $scope.$watch('query', function (value) {
-    regex = new RegExp('\\b' + value.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1"), 'i');
-  });
-  $scope.filterBySearch = function(person) {
-      return ($scope.query
-        && $scope.query.length >= 2
-        && regex.test(person.forename + ' ' + person.surname));
-  };
-  $scope.panTo = function(person) {
-    $scope.currentPerson = person;
+app.controller('HomeCtrl', function ($scope, Search) {
+  $scope.results = [];
+  $scope.search = function(term) {
+    var queryId = Search.query(term);
+    Search.results(queryId, function(data){
+      $scope.results = data;
+    });
   };
 });
--- app/scripts/services/search.js
+++ app/scripts/services/search.js
@@ -1,35 +1,26 @@
 'use strict';
 
-/*
 app.factory('Search',
   function ($firebase, FIREBASE_URL) {
-    var ref = new Firebase(FIREBASE_URL + 'search/request');
-    var requests = $firebase(ref).$asArray();
+    var searchRef = new Firebase(FIREBASE_URL + 'search');
+    var requestRef = searchRef.child("request");
+    var responseRef = searchRef.child("response");
     var Search = {
-      create: function (request) {
-        return requests.$add(request);
-      }
-    };
-    return Search;
-  });
-*/
-
-app.factory('Search',
-  function ($firebase, FIREBASE_URL) {
-    var queue = new Firebase(FIREBASE_URL + 'search');
-    var Search = {
-      query: function (term, callback) {
-        var reqRef = queue.child('request').push({ index: 'snappynumber', type: 'person', query: term });
-        queue.child('response/'+reqRef.name()).on('value', function fn(snap) {
+      //allRequests: requests,
+      //allResponses: responses,
+      query: function (term) {
+        //var id = requestRef.push({index: 'sn-persons', type: 'person', query: term}).name();
+        return requestRef.push({index: 'sn-persons', type: 'person', query: term}).name();
+      },
+      results: function (requestId, callback) {
+        var response = responseRef.child(requestId).on('value', function fn(snap) {
           if(snap.val() !== null) {
-             snap.ref().off('value', fn);
-             snap.ref().remove();
-             if(callback)
-             {
-               callback(snap.val());
-             }
+            snap.ref().off('value', fn);
+            snap.ref().remove();
+            callback(snap.val());
+            requestRef.child(requestId).remove();
           }
-       });
+        });
       }
     };
     return Search;
