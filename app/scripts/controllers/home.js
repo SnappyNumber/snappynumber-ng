8e3c1d3beaabea8e702cf49f7cebeb12fa668898
Rob Thijssen <rthijssen@gmail.com>
2014-09-05 17:03:47 +0100

    corrected query syntax/structure

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -4,8 +4,6 @@ app.controller('HomeCtrl', function ($scope, Search) {
   $scope.results = [];
   $scope.search = function(term) {
     var queryId = Search.query(term);
-    Search.results(queryId, function(data){
-      $scope.results = data;
-    });
+    $scope.results = Search.results(queryId);
   };
 });
--- app/scripts/services/search.js
+++ app/scripts/services/search.js
@@ -1,7 +1,7 @@
 'use strict';
 
 app.factory('Search',
-  function ($firebase, FIREBASE_URL) {
+  function ($firebase, FIREBASE_URL, $q) {
     var searchRef = new Firebase(FIREBASE_URL + 'search');
     var requestRef = searchRef.child("request");
     var responseRef = searchRef.child("response");
@@ -9,18 +9,32 @@ app.factory('Search',
       //allRequests: requests,
       //allResponses: responses,
       query: function (term) {
-        //var id = requestRef.push({index: 'sn-persons', type: 'person', query: term}).name();
-        return requestRef.push({index: 'sn-persons', type: 'person', query: term}).name();
+        var request = {
+          index: 'sn-persons',
+          type: 'person',
+          query: {
+            fuzzy_like_this:{
+              fields: ["forename", "surname"],
+              like_text: term
+            }
+          }
+        };
+        return requestRef.push(request).name();
       },
-      results: function (requestId, callback) {
-        var response = responseRef.child(requestId).on('value', function fn(snap) {
+      results: function (requestId) {
+        var deferred = $q.defer();
+        responseRef.child(requestId).on('value', function fn(snap) {
           if(snap.val() !== null) {
+            var searchResults = snap.val().hits;
+            deferred.resolve(searchResults);
             snap.ref().off('value', fn);
             snap.ref().remove();
-            callback(snap.val());
             requestRef.child(requestId).remove();
+          } else {
+            deferred.reject('failed: ' + requestId);
           }
         });
+        return deferred.promise;
       }
     };
     return Search;
